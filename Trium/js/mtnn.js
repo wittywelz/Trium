1e3d3679e80590cc0ca58aad5b2405760996769b
wittywelz <43678518+wittywelz@users.noreply.github.com>
2018-09-28 17:51:01 +0100

    Add files via upload

---
+++ Trium/js/mtnn.js
@@ -0,0 +1,205 @@
+<!doctype html>
+<html>
+<head>
+	<title>MTNN</title>
+	<link rel="stylesheet" type="text/css" href="style.css"  media="screen">
+	<link rel="stylesheet" type="text/css" href="style2.css"  media="screen">
+	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<link rel="stylesheet" href="css/bootstrap.min.css">
+	<link rel="stylesheet" href="css/owl.carousel.min.css">
+	<link rel="stylesheet" href="fonts/linear/lnr-style.css">
+	<link rel="stylesheet" href="css/owl.theme.default.css">
+	<link rel="stylesheet" href="css/custom.css">
+	<script type="text/javascript" src="js/mtnn.js"></script>
+	    <script src="js/respond.js"></script>
+<style type="text/css">
+	.owl-theme{
+	position: relative;
+}
+	.owl-theme .owl-dots {
+    text-align: center;
+    -webkit-tap-highlight-color: transparent;
+    position: absolute;
+    top: 90%;
+    left: 50%;
+}
+		.owl-theme .owl-dots .owl-dot span {
+    width: 13px;
+    height: 13px;
+    margin: 7px 9px;
+    background: #c4c4c4;
+    display: block;
+  }
+
+.owl-carousel {
+    z-index: -1;
+    top: -93px;
+}
+
+.owl-theme .owl-dots .owl-dot.active span, .owl-theme .owl-dots .owl-dot:hover span {
+    background: #ffbe00;
+}
+
+
+</style>
+</head>
+<body class="main">
+	<div class="wrapper">
+
+			<div class="wrapper"> 
+					<div class="row1">   <!--row1 div start here -->
+
+		
+					<div class="col-md-6 col-sm-6 list1">
+						<ul>
+							<li class="li1"><a href="#">lorem ipsum 1</a></li>
+							<li class="li2"><a href="#">lorem ipsum 2</li></a>
+							<li class="li2"><a href="#">lorem ipsum 3</li></a>
+						</ul>
+					</div>
+
+					<div class="col-md-6 col-sm-6 list2">
+						<ul>
+							<li><a href="#"><img class="img-circle" src="image\fum.jpg"></a></li>
+							<li><p class="t1">User Name</p></li>
+							<li class="li2"><a href="#"><i class="fa fa-cog" ></i></a></li>
+							<li class="li2"><a href="#"><i class="fa fa-bell"></i></a></li>
+						</ul>
+					</div>
+
+			</div>   <!--row1 div ends here-->
+			
+			<div>
+				<div class="nav2">
+						<div class="col-sm-6 col-xs-6 list11">                                                                
+							<ul>
+								<li class="li2"><a href="#"><strong>browse</strong></a></li>
+								<li class="li2"><a href="#"><strong>page</strong></li></a>
+								
+							</ul>
+						</div>
+
+						<div class="col-sm-6 col-xs-6 list22">
+							<ul>
+								<li class="li2"><a href="#"><i class="fa fa-pencil" ></i></a></li>
+								<li class="li2"><a href="#"><i class="fa fa-star" ></i></a></li>
+								<li class="li2"><a href="#"><i class="fa fa-share-alt"></i></a></li>
+							</ul>
+						</div>
+						
+					</div>
+					
+
+					<div class="menu">
+							<nav class="navbar navbar-inverse noback">
+								<div class="container-fluid">
+									<div class="navbar-header">
+										<button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar1">
+											<span class="sr-only">Toggle navigation</span>
+											<span class="icon-bar"></span>
+											<span class="icon-bar"></span>
+											<span class="icon-bar"></span>
+										</button>
+										<a class="navbar-brand" href="#"><img class="img-responsive" src="image/logo.png" alt="logo">
+										</a>
+									</div>
+								<div id="navbar1" class="navbar-collapse collapse">
+									<ul class="nav navbar-nav">
+										<li><a class="t2" href="#">Home</a></li>
+										<li><a class="t2" href="#">lorem ipsum</a></li>
+										<li><a class="t2" href="#">lorem ipsum</a></li>
+										<li><a class="t2" href="#">lorem ipsum</a></li>
+										<li><a class="t2" href="#">lorem ipsum</a></li>
+									</ul>
+									
+								<div class="input-group pull-right">
+										<input type="text" class="form-control" placeholder="ALL SITES" name="srch-term" id="srch-term">
+										<div class="input-group-btn">
+											<button class="btn li2" type="submit"><span class="lnr lnr-magnifier flipped"><span></button>
+											</div>
+										</div>
+										
+								</div>
+
+									
+									<!--/.nav-collapse -->
+								</div>
+								<!--/.container-fluid -->
+								
+
+							</nav>
+					</div>
+
+					<div class="owl-carousel owl-theme owl-loaded owl-drag" id="owl-carousel">
+				
+							<div class="row item"> 
+								<img class="img-responsive" src="image/banner.jpg" alt="">
+								<div class="">
+									<h1 class="t4">lorem ipsum <br/> dolor sit amet</h1>
+									<p class="t5">lorem ipsum dolot sit amet, consetetur<br/> sed diam nonumy eirmod tempor invidunt</p>
+									<button class="btn btnn" type="button" value="Find out more"><b>Find out more</b></button>
+								</div>
+							</div>
+						
+							<div class="row item"> 
+								<img class="img-responsive" src="image/pix7.jpg" alt="">
+								<div class="">
+									<h1 class="t4">lorem ipsum <br/> dolor sit amet</h1>
+									<p class="t5">lorem ipsum dolot sit amet, consetetur<br/> sed diam nonumy eirmod tempor invidunt</p>
+									<button class="btn btnn" type="button" value="Find out more"><b>Find out more</b></button>
+								</div>
+							</div>
+
+							<div class="row item"> 
+								<img class="img-responsive" src="image/pix2.jpg">
+								<div class="">
+									<h1 class="t4">lorem ipsum <br/> dolor sit amet</h1>
+									<p class="t5">lorem ipsum dolot sit amet, consetetur<br/> sed diam nonumy eirmod tempor invidunt</p>
+									<button class="btn btnn" type="button" value="Find out more"><b>Find out more</b></button>
+								</div>
+							</div>
+
+							<div class="row item">
+								<img class="img-responsive" src="image\pix1.jpg">
+								<div class="">
+									<h1 class="t4">lorem ipsum <br/> dolor sit amet</h1>
+									<p class="t5">lorem ipsum dolot sit amet, consetetur<br/> sed diam nonumy eirmod tempor invidunt</p>
+									<button class="btn btnn" type="button" value="Find out more"><b>Find out more</b></button>									
+								</div>							
+							</div>	
+					</div>
+
+
+			</div>    <!--row2 div ends here-->
+
+
+
+
+	</div>  <!--wrapper div -->
+
+<!-- javascript  -->
+	<script src="js/jquery.js"></script>
+
+	<script src="js/owl.carousel.min.js"></script>
+	<script type="text/javascript">
+		$(document).ready(function(){
+		  $("#owl-carousel").owlCarousel({
+		  center: true,
+		  loop:true,
+	      margin:0,
+	      navSpeed:5000,
+	      nav:false,
+	      items:1,
+	      autoplay:true,
+	      autoplayTimeout:10000,
+	      autoplayHoverPause:true,
+	      dotsSpeed:1000,
+	      
+		  });
+		});
+	</script>
+		<script src="js/bootstrap.min.js"></script>
+</body>
+</html>
